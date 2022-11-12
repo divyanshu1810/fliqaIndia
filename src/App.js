89b3973547f27be33eaf7882993e342213b7d2fb
Divyanshu Yadav <divyanshu.1810@gmail.com>
2022-11-12 16:23:06 +0530

    :lipstick: Footer Added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Landing from './components/Landing';
 
@@ -7,6 +8,7 @@ function App() {
     <div>
       <Header/>
       <Landing/>
+      <Footer/>
     </div>
   );
 }
